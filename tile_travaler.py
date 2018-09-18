8b2b62373db6eba927adca2bc0b60ca4b356c6f6
ragnar <ragnarr18@skel.ru.is>
2018-09-18 16:00:05 +0000

    latest version

--- tile_travaler.py
+++ tile_travaler.py
@@ -61,73 +61,9 @@ while not victory:
             print("(S)outh or (W)est.")
             possible_dir= 'sw'
 
-
+print('')
 print("Victory!") 
-    # elif counter_for_x ==1 and counter_for_y ==3:
-    #     print("You can travel: (E)ast or (S)outh.")
-    #     possible_dir = 'seSE'
-    #     direction=input("Direction: ")
-    #     if direction == 's' or direction =='S' and direction in possible_dir:
-    #         counter_for_y -=1
-    #     elif direction == 'e' or direction == 'E' and direction in possible_dir:
-    #         counter_for_x +=1 
-    #     else:
-    #         print("Not a valid direction!")
-    # elif counter_for_x ==2 and counter_for_y == 1:
-    #     print("You can travel: (N)orth.")
-    #     possible_dir = 'nN'
-    #     direction=input("Direction: ")
-    #     if direction == 'n' or direction =='N' and direction in possible_dir:
-    #         counter_for_y +=1
-    #     else:
-    #         print("Not a valid direction!")
-    # elif counter_for_x ==2 and counter_for_y == 2:
-    #     print("You can travel: (W)est or (S)outh.")
-    #     possible_dir = 'swSW'
-    #     direction=input("Direction: ")
-    #     if direction == 's' or direction == 'S' and direction in possible_dir:
-    #         counter_for_y -=1
-    #     elif direction == 'w' or direction == 'W' and direction in possible_dir:
-    #         counter_for_x -=1 
-    #     else:
-    #         print("Not a valid direction")
-
-    # elif counter_for_x ==2 and counter_for_y ==3:
-    #     print("You can travel: (E)ast or (W)est.")
-    #     possible_dir = 'weWE'
-    #     direction=input("Direction: ")
-    #     if direction == 'w' or direction =='W' and direction in possible_dir:
-    #         counter_for_x -=1
-    #     elif direction == 'e' or direction == 'E' and direction in possible_dir:
-    #         counter_for_x +=1
-    #     else:
-    #         print("Not a valid direction!") 
-
-    # elif counter_for_x ==3 and counter_for_y ==1:
-    #     print("Victory!")
-    #     victory = True
     
-    # elif counter_for_x ==3 and counter_for_y ==2:
-    #     print("You can travel: (N)orth or (S)outh.")
-    #     possible_dir = 'snSN'
-    #     direction=input("Direction: ")
-    #     if direction == 's' or direction=='S' and direction in possible_dir:
-    #         counter_for_y -=1
-    #     elif direction == 'n'or direction =='N' and direction in possible_dir:
-    #         counter_for_y +=1 
-    #     else:
-    #         print("Not a valid direction")
-    # elif counter_for_x ==3 and counter_for_y ==3:
-    #     print("You can travel: (W)est or (S)outh.")
-    #     possible_dir = 'wsWS'
-    #     direction=input("Direction: ")
-    #     if direction =='s' or direction == 'S' and direction in possible_dir:
-    #         counter_for_y-=1
-    #     elif direction =='w' or direction == 'W' and direction in possible_dir:
-    #         counter_for_x -=1
-    #     else:
-    #         print("Not a valid direction!")
-        
     
 
 
