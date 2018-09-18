b7057a616fa7f924412738aeecbcad33948695c5
ragnar <ragnarr18@skel.ru.is>
2018-09-18 16:42:10 +0000

    last version

--- tile_travaler.py
+++ tile_travaler.py
@@ -3,7 +3,7 @@ counter_for_y = 1
 
 victory = False
 
-
+ 
 possible_dir = 'n'
 
 print("You can travel: (N)orth.")
@@ -24,46 +24,56 @@ while not victory:
         else:
             counter_for_x -= 1
 
-        print("You can travel: ", end='')
+       
             
         if counter_for_x ==1 and counter_for_y == 1 :
-            print("(N)orth.")
+            print("You can travel: (N)orth.")
             possible_dir = 'n'
         
         elif counter_for_x ==1 and counter_for_y ==2:
-            print("(N)orth or (E)ast or (S)outh.")
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
             possible_dir = 'nes'
         
         elif counter_for_x ==1 and counter_for_y ==3:
-            print("(E)ast or (S)outh.")
+            print("You can travel: (E)ast or (S)outh.")
             possible_dir = 'es'
         
         elif counter_for_x ==2 and counter_for_y ==1:
-            print("(N)orth.")
+            print("You can travel: (N)orth.")
             possible_dir = 'n'
         
         elif counter_for_x ==2 and counter_for_y ==2:
-            print("(S)outh or (W)est.")
+            print("You can travel: (S)outh or (W)est.")
             possible_dir = 'ws'
 
         elif counter_for_x ==2 and counter_for_y ==3:
-            print("(E)ast or (W)est.")
+            print("You can travel: (E)ast or (W)est.")
             possible_dir= 'ew'
         
         elif counter_for_x ==3 and counter_for_y==1:
+            
             victory = True
+           
+            
+
             
         elif counter_for_x == 3 and counter_for_y ==2:
-            print("(N)orth or (S)outh.")
+            print("You can travel: (N)orth or (S)outh.")
             possible_dir = 'sn'
         
         elif counter_for_x == 3 and counter_for_y ==3:
-            print("(S)outh or (W)est.")
+            print("You can travel: (S)outh or (W)est.")
             possible_dir= 'sw'
 
+print("Victory!")
 
-print("Victory!",sep='') 
-    
+
+
+
+
+
+
+
     
 
 
