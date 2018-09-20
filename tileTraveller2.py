313c3ba8f6fae85d70180976c6c5e79d7c150d00
ragnar <ragnarr18@skel.ru.is>
2018-09-20 13:33:55 +0000

    if statement for victory added

--- tileTraveller2.py
+++ tileTraveller2.py
@@ -23,13 +23,6 @@ def east(direction):
     else:
         return False
 
-def finish(victory):
-    if victory:
-        return "Victory!" 
-    
-
-
-
 possible_directions = 'n'
 
 counter_for_x = 1 
@@ -37,7 +30,6 @@ counter_for_y = 1
 
 victory = False
 print("You can travel: (N)orth.")
-#print("You can travel: (N)orth.")
 while not victory:
     
     direction = input("Direction: ")
@@ -54,33 +46,34 @@ while not victory:
     if not direction in possible_directions:
         print("Not a valid direction!")
     else:
-        print("You can travel: ",end='')
-    
-        if counter_for_x ==1 and counter_for_y ==2:
-            possible_directions = 'nes'
-            print("(N)orth or (E)ast or (S)outh." )
-        elif counter_for_x ==1 and counter_for_y ==3:
-            possible_directions = 'se'
-            print("(E)ast or (S)outh.")
-        elif counter_for_x ==2 and counter_for_y ==1:
-            possible_directions = 'n'
-            print("(N)orth.")
-        elif counter_for_x ==2 and counter_for_y ==2:
-            possible_directions = 'sw'
-            print("(S)outh or (W)est.")
-        elif counter_for_x ==2 and counter_for_y ==3:
-            possible_directions = 'ew'
-            print("(E)ast or (W)est.")
-        elif counter_for_x ==3 and counter_for_y ==1:
-            victory = True 
-            break
-            
-        elif counter_for_x ==3 and counter_for_y ==2:
-            possible_directions = 'ns'
-            print("(N)orth or (S)outh.")
-        elif counter_for_x ==3 and counter_for_y ==3:
-            possible_directions = 'ws'
-            print(("S)outh or (W)est.")
-    
+        if counter_for_x ==3 and counter_for_y ==1:
+            print("Victory!") 
+            victory = True
+        else:
+        
+            print("You can travel: ",end='')
 
-print("Victory!")
+            if counter_for_x ==1 and counter_for_y ==2:
+                possible_directions = 'nes'
+                print("(N)orth or (E)ast or (S)outh." )
+            elif counter_for_x ==1 and counter_for_y ==3:
+                possible_directions = 'se'
+                print("(E)ast or (S)outh.")
+            elif counter_for_x ==2 and counter_for_y ==1:
+                possible_directions = 'n'
+                print("(N)orth.")
+            elif counter_for_x ==2 and counter_for_y ==2:
+                possible_directions = 'sw'
+                print("(S)outh or (W)est.")
+            elif counter_for_x ==2 and counter_for_y ==3:
+                possible_directions = 'ew'
+                print("(E)ast or (W)est.")
+                
+            elif counter_for_x ==3 and counter_for_y ==2:
+                possible_directions = 'ns'
+                print("(N)orth or (S)outh.")
+            elif counter_for_x ==3 and counter_for_y ==3:
+                possible_directions = 'ws'
+                print(("S)outh or (W)est.")
+
+                
